ed1aacc5a918b42cda8bb34b20986e4d23eb4150
Karitz <krnrodea@gmail.com>
2024-03-18 11:41:05 -0600

    Segundo commit

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnIniciar;
+    EditText user, pass;
+    int sum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIniciar = (Button) findViewById(R.id.Iniciar);
+        user = (EditText)findViewById(R.id.User);
+        pass = (EditText)findViewById(R.id.Pass);
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                if(sum<3){
+                    sum = sum + 1;
+
+                    Intent intent = new Intent(getApplicationContext(), Menu.class);
+                    String datos = user.getText().toString();
+                    String contr = pass.getText().toString();
+
+                    if(datos.equals("SuperUsuario1") & contr.equals("UsuarioPass24")){
+                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(), "Bienvenido al Menú "+datos, Toast.LENGTH_LONG).show();
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Cuenta o contraseña incorrectos, te quedan:  " +(3-sum)+" Intentos",Toast.LENGTH_SHORT).show();
+                        }
+                }else{
+                    Toast.makeText(getApplicationContext(),"Excediste tus oportunidades",Toast.LENGTH_SHORT).show();
+                    btnIniciar.setEnabled(false);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication1/Menu.java
+++ app/src/main/java/com/example/myapplication1/Menu.java
@@ -2,13 +2,53 @@ package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
 
+    Button Figuras, Galeria, Quiz, regresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Figuras = (Button)findViewById(R.id.Figuras);
+        Galeria =(Button)findViewById(R.id.Galeria);
+        Quiz =(Button)findViewById(R.id.Quiz);
+        regresar = (Button) findViewById(R.id.Regresar);
+
+        Figuras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AreaVolumen.class);
+                startActivity(intent);
+            }
+        });
+
+        Galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Galeria.class);
+                startActivity(intent);
+            }
+        });
+
+        Quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Juego.class);
+                startActivity(intent);
+            }
+        });
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
