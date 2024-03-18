ce66b1e7e0be87f42849b4ead07be6e470977988
Karitz <krnrodea@gmail.com>
2024-03-18 16:33:45 -0600

    Tercer commit

--- app/src/main/java/com/example/myapplication1/Galeria.java
+++ app/src/main/java/com/example/myapplication1/Galeria.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.interfaces.ItemClickListener;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class Galeria extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galeria);
-    }
+
+        ImageSlider imageSlider = findViewById(R.id.Images);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.images__63_, "Cielo" ,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.images__61_, "Figuras" ,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.images__62_, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.wp8968806, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+
+
+        }
 }
