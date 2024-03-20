ff3d593f410c0493cb01d7713f2c60df35936b80
Karitz <krnrodea@gmail.com>
2024-03-20 03:13:18 -0600

    Undecimo commit

--- app/src/main/java/com/example/myapplication1/AreaVolumen.java
+++ app/src/main/java/com/example/myapplication1/AreaVolumen.java
@@ -2,13 +2,40 @@ package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AreaVolumen extends AppCompatActivity {
+    Button Circ, Trian, PRec, Cubo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area_volumen);
+
+        Circ = (Button) findViewById(R.id.Circulo);
+        Trian = (Button) findViewById(R.id.Triangulo);
+        PRec = (Button) findViewById(R.id.PrismaR);
+        Cubo = (Button) findViewById(R.id.Cubo);
+
+        Circ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Circulo.class);
+                startActivity(i);
+            }
+        });
+
+        Trian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Triangulo.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
