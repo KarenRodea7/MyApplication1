06049fbf2415b16dc6b4b67636de34ed9e457c98
Karitz <krnrodea@gmail.com>
2024-03-20 09:34:27 -0600

    Nuevo commit

--- app/src/main/java/com/example/myapplication1/AreaVolumen.java
+++ app/src/main/java/com/example/myapplication1/AreaVolumen.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class AreaVolumen extends AppCompatActivity {
-    Button Circ, Trian, PRec, Cubo;
+    Button Circ, Trian, PRec, Cubo, Salir, Menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +19,9 @@ public class AreaVolumen extends AppCompatActivity {
         Trian = (Button)findViewById(R.id.Triangulo);
         PRec = (Button)findViewById(R.id.PrismaR);
         Cubo = (Button)findViewById(R.id.Cubo);
+
+        Salir = (Button)findViewById(R.id.SalirFig);
+        Menu = (Button)findViewById(R.id.MenuFig);
 
         Circ.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +52,24 @@ public class AreaVolumen extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), com.example.myapplication1.Cubo.class);
                 startActivity(i);
+            }
+        });
+
+        Menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Menu.class);
+                startActivity(i);
+            }
+        });
+
+        Salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
         });
 
