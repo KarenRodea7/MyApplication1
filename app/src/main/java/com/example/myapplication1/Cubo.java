37263a2e9fa504a542e84f74798715932aca51c3
Karitz <krnrodea@gmail.com>
2024-03-20 07:24:36 -0600

    Decimosexto commit - Figuras

--- app/src/main/java/com/example/myapplication1/Cubo.java
+++ app/src/main/java/com/example/myapplication1/Cubo.java
@@ -18,8 +18,21 @@ public class Cubo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cubo);
 
+        EditText lado = (EditText)findViewById(R.id.lado);
+        Button calc = (Button)findViewById(R.id.CalcCu);
+        TextView res = (TextView)findViewById(R.id.ResCu);
+
         salir = (Button)findViewById(R.id.SalirCubo);
         regresar =(Button)findViewById(R.id.MenuCubo);
+
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double lad = Double.parseDouble(lado.getText().toString());
+                double area = Math.pow(lad,3);
+                res.setText("Área: "+area);
+            }
+        });
 
         regresar.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/myapplication1/PrisRectang.java
+++ app/src/main/java/com/example/myapplication1/PrisRectang.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PrisRectang extends AppCompatActivity {
 
@@ -16,8 +18,25 @@ public class PrisRectang extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pris_rectang);
 
+        EditText lado = (EditText)findViewById(R.id.LadoPR);
+        EditText base = (EditText)findViewById(R.id.BasePR);
+        EditText altura = (EditText)findViewById(R.id.AltPR);
+        Button calc = (Button)findViewById(R.id.CalcPR);
+        TextView res = (TextView)findViewById(R.id.ResPR);
+
         salir = (Button)findViewById(R.id.SalirPRec);
         regresar =(Button)findViewById(R.id.MenuPRec);
+
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double lad = Double.parseDouble(lado.getText().toString());
+                Double bas = Double.parseDouble(base.getText().toString());
+                Double alt = Double.parseDouble(altura.getText().toString());
+                double area = (bas*lad)*alt;
+                res.setText("Área: "+area);
+            }
+        });
 
         regresar.setOnClickListener(new View.OnClickListener() {
             @Override
