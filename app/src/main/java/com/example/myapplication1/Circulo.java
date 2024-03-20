33ed94059e2b8a1d55a2feaea0611431eadaf054
Karitz <krnrodea@gmail.com>
2024-03-20 07:05:53 -0600

    Decimoquinto commit

--- app/src/main/java/com/example/myapplication1/Circulo.java
+++ app/src/main/java/com/example/myapplication1/Circulo.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Circulo extends AppCompatActivity {
 
@@ -15,8 +17,22 @@ public class Circulo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circulo);
 
+        EditText radio = (EditText)findViewById(R.id.Radio);
+        Button calc = (Button)findViewById(R.id.CalcCir);
+        TextView res = (TextView)findViewById(R.id.ResCir);
+
         salir = (Button)findViewById(R.id.SalirCir);
         regresar =(Button)findViewById(R.id.MenuCir);
+
+
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double rad = Double.parseDouble(radio.getText().toString());
+                double area = Math.pow(rad,2)*3.1416;
+                res.setText("Área: "+area);
+            }
+        });
 
         regresar.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/myapplication1/Triangulo.java
+++ app/src/main/java/com/example/myapplication1/Triangulo.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Triangulo extends AppCompatActivity {
 
@@ -16,8 +18,24 @@ public class Triangulo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_triangulo);
 
+        EditText altura = (EditText)findViewById(R.id.AltT);
+        EditText base = (EditText)findViewById(R.id.BaseT);
+        Button calc = (Button)findViewById(R.id.CalcT);
+        TextView res = (TextView)findViewById(R.id.ResT);
+
         salir = (Button)findViewById(R.id.SalirTri);
         regresar =(Button)findViewById(R.id.MenuTri);
+
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double alt = Double.parseDouble(altura.getText().toString());
+                Double bas = Double.parseDouble(base.getText().toString());
+                double area = (bas*alt)/2;
+                res.setText("Área: "+area);
+            }
+        });
+
         regresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
